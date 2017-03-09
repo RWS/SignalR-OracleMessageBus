3ee9f88cb09b87fb23ed8f1d334ee2097b6027df
GLOBAL\lsiddiquee <lsiddiquee@sdl.com>
2017-03-09 16:09:37 +0100

    removing missing snk file in project

--- samples/SDL.SignalR.OracleMessageBus.Server/Program.cs
+++ samples/SDL.SignalR.OracleMessageBus.Server/Program.cs
@@ -18,7 +18,7 @@ namespace Sdl.SignalR.OracleMessageBus.Server
             string url = "http://localhost:8888/";
             using (WebApp.Start<Startup>(url))
             {
-                Console.WriteLine("CME Server running at {0}", url);
+                Console.WriteLine("Backend Server running at {0}", url);
                 Console.ReadLine();
             }
 
