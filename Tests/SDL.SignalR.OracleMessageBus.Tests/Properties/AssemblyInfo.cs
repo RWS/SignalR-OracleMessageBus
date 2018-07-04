ee123102e5fcce22c6fecd7e47fff3904a94e583
ashatrov <ashatrov@sdl.com>
2018-07-04 10:26:30 +0300

    Updated several nuget packages.

--- Tests/SDL.SignalR.OracleMessageBus.Tests/Properties/AssemblyInfo.cs
+++ Tests/SDL.SignalR.OracleMessageBus.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SDL.SignalR.OracleMessageBus.Tests")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SDL.SignalR.OracleMessageBus.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8337cef5-7a02-4ff8-bb45-14038e17de8d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/SDL.SignalR.OracleMessageBus/Properties/AssemblyInfo.cs
+++ src/SDL.SignalR.OracleMessageBus/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Sdl.SignalR.OracleMessageBus")]
-[assembly: AssemblyDescription("Oracle messaging backplane for scaling out of ASP.NET SignalR applications in a web-farm.")]
-[assembly: AssemblyCompany("SDL Group")]
-[assembly: AssemblyProduct("Sdl.SignalR.OracleMessageBus")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,18 +11,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("286e7ded-376d-45a2-a1a6-e2dfc1431360")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("Sdl.SignalR.OracleMessageBus.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a793292849ba1a1e85219d8f741ba2daea53e2bd5da69b6209082c5f001685ba9053260ffef5258519b28a10bedf24341b594dc9092c699c871bc3e0fd86777f2dd0dd7e918432f3a7d6c89b1b51fdfea5026fbc8d47db378bfe533669cc7a9b6f76adf8585cea48abf45f0b96a2ed0ab3d94074017a8c505aa787bf1d7751d0")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a793292849ba1a1e85219d8f741ba2daea53e2bd5da69b6209082c5f001685ba9053260ffef5258519b28a10bedf24341b594dc9092c699c871bc3e0fd86777f2dd0dd7e918432f3a7d6c89b1b51fdfea5026fbc8d47db378bfe533669cc7a9b6f76adf8585cea48abf45f0b96a2ed0ab3d94074017a8c505aa787bf1d7751d0")]
